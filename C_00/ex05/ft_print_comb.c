01cc8d4aacfe02d788717c58fb82daf513e9d4f3
Felippe De alencar silva <fde-alen@student.42sp.org.br>
2023-06-09 15:22:59 -0300

    jun9b

---
+++ C_00/ex05/ft_print_comb.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 18:30:51 by fde-alen          #+#    #+#             */
+/*   Updated: 2023/03/15 11:06:00 by fde-alen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_3_digits(int i, int j, int k)
+{
+	ft_putchar(i + '0');
+	ft_putchar(j + '0');
+	ft_putchar(k + '0');
+	if (i != 7 || j != 8 || k != 9)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i + 1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				ft_print_3_digits(i, j, k);
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_putchar('\n');
+}
